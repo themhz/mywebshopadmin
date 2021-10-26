7e302c3cf67846571198d012a430dc69421c7437
themhz <themhz@gmail.com>
2021-10-27 01:30:20 +0300

    editing and saving menu functionality

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,6 +23,36 @@
     <link rel="stylesheet" href="assets/css/responsive.css">
     <!-- modernizr css -->
     <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+    <script src="js/menu.js"></script>
+    <!-- offset area end -->
+    <!-- jquery latest version -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js" defer></script>
+
+    <!-- start chart js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!-- start zingchart js -->
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+    <script>
+        zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+        ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
+    </script>
+    <!-- all line chart activation -->
+    <script src="assets/js/line-chart.js"></script>
+    <!-- all pie chart -->
+    <script src="assets/js/pie-chart.js"></script>
+    <!-- others plugins -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js" defer></script>
 </head>
 
 <body>
@@ -529,66 +559,5 @@
         </div>
     </div>
 </div>
-<script src="js/menu.js"></script>
-<!-- offset area end -->
-<!-- jquery latest version -->
-<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
-<!-- bootstrap 4 js -->
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/owl.carousel.min.js"></script>
-<script src="assets/js/metisMenu.min.js"></script>
-<script src="assets/js/jquery.slimscroll.min.js"></script>
-<script src="assets/js/jquery.slicknav.min.js" defer></script>
-
-<!-- start chart js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-<!-- start highcharts js -->
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<!-- start zingchart js -->
-<script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-<script>
-    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-</script>
-<!-- all line chart activation -->
-<script src="assets/js/line-chart.js"></script>
-<!-- all pie chart -->
-<script src="assets/js/pie-chart.js"></script>
-<!-- others plugins -->
-<script src="assets/js/plugins.js"></script>
-<script src="assets/js/scripts.js" defer></script>
-
-<script defer>
-    let counter = 0;
-    $(document).ready(function(){
-
-        $('#nestedDemo').sortable();
-        $('#nestedDemo11').sortable();
-        $('#nestedDemo12').sortable();
-        //$('.list-group-item nested-1').sortable();
-
-
-        //dragable($('#nestedDemo'));
-
-        //alert(1);
-        //console.log($('#nestedDemo').)
-        //$('#nestedDemo').sortable();
-        // for (var i = 0; i < nestedSortables.length; i++) {
-        //     new Sortable(nestedSortables[i], {
-        //         group: 'nested',
-        //         animation: 150,
-        //         fallbackOnBody: true,
-        //         swapThreshold: 0.65
-        //     });
-        // }
-    });
-
-    function dragable(elem){
-        elem.children.forEach(function(item) {
-           console.log(item);
-        });
-    }
-</script>
 </body>
 </html>
